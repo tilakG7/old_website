1aedda0244b70ee281b2b44d201971458d0e6443
TilakG7 <shagungupta@Shaguns-MacBook-Pro.local>
2017-01-24 01:49:30 -0500

    project update

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,6 +157,19 @@
 						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
 					});
 
+					$('#project4').poptrox({
+						caption: function($a) { return $a.next('h3').text(); },
+						overlayColor: '#2c2c2c',
+						overlayOpacity: 0.85,
+						popupCloserText: '',
+						popupLoaderText: '',
+						selector: '.work-item a.image',
+						usePopupCaption: true,
+						usePopupDefaultStyling: false,
+						usePopupEasyClose: false,
+						usePopupNav: true,
+						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+					});
 
 				});
 
